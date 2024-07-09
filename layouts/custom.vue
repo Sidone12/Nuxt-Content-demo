e91b0ce1a8c5141ab665eb87f2d0685fe9d3c27f
Oleksii <sidone666@gmail.com>
2024-07-09 21:11:55 +0300

    add layouts

---
+++ layouts/custom.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>This is another layout!</h1>
+        <nav>
+            <ul>
+                <li>
+                    <nuxt-link to="/">Home</nuxt-link>
+                </li>
+            </ul>
+        </nav>
+        <slot />
+    </div>
+    </template>
---
+++ layouts/default.vue
@@ -0,0 +1,45 @@
+<script setup>
+useHead({
+    titleTemplate: '%s - Oleksii Portfolio',
+    link: [{
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+            crossorigin: ''
+        }
+    ]
+})
+</script>
+
+<template>
+<div class="container mx-auto max-w-6xl">
+    <header class="flex justify-between items-center pt-5">
+        <div class="flex items-center space-x-12">
+            <div>
+                <nuxt-link class="hover:bg-gray-200 text-xl font-semibold p-2 " to="/">Oleksii Katerynchuk</nuxt-link>
+            </div>
+            <Menu/>
+        </div>
+       <ClientOnly >
+        <ColorModeSelector />
+       </ClientOnly>
+    </header>
+    <main class="p-2 mt-10">
+
+        <slot />
+    </main>
+</div>
+</template>
+
+<style>
+body {
+    font-family: 'Roboto';
+}
+
+body {
+    @apply bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300
+}
+</style>
