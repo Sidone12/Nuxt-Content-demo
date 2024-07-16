38f36c23f4bf121f229b2a1fb95b97bd2dc80003
Oleksii <sidone666@gmail.com>
2024-07-16 19:42:48 +0300

    add tilewindcss colormode ext. +button

---
+++ components/color-mode-selector.vue
@@ -0,0 +1,42 @@
+<template>
+<div class=" flex space-x-2 items-center">
+    <div class="text-gray-500 text-xs" v-if="showNextModelLabel">
+        Change to {{ nextMode }}
+    </div>
+    <button @click="toggleMode" @mouseenter="showNextModelLabel=true" @mouseleave="showNextModelLabel = false" class="hover:bg-gray-200 dark:hover:bg-gray-600 px-2 py-1 text-gray-500">
+            {{ nextModeIcon }}
+        </button>
+</div>
+</template>
+
+<script setup lang="ts">
+const showNextModelLabel = ref < boolean > (false);
+
+const colorMode = useColorMode()
+
+type Mode = 'system' | 'dark' | 'light';
+const modes: Mode[] = ['system', 'dark', 'light', ];
+
+const nextModeIcons = {
+    system: '🌓',
+    dark: '🌑',
+    light: '🌕'
+}
+const nextMode = computed(() => {
+    const currentModeIndex = modes.indexOf(colorMode.preference);
+    let nextModeIndex = null
+    if (currentModeIndex + 1 === modes.length) {
+        nextModeIndex = 0
+    } else {
+        nextModeIndex = currentModeIndex + 1
+    }
+    return modes[nextModeIndex]
+})
+
+const nextModeIcon = computed(() => nextModeIcons[nextMode.value])
+const toggleMode = () => colorMode.preference = nextMode.value
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode'
+  ],
+  colorMode: {
+    classSuffix: ''
+  },
+  compatibilityDate: "2024-07-03"
 })
