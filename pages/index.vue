1c57139ed20c769d9f14ecdd6758df83cb73478d
Oleksii <sidone666@gmail.com>
2024-07-16 19:45:38 +0300

    add meta

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<script setup>
+useSeoMeta({
+  title: ' Main page',
+  description: ' My Nuxt Portfolio',
+  ogTitle: 'Oleksii Portfolio',
+  ogDescription: 'Web dew Portfolio',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: 'Oleksii Portfolio',
+  twitterDescription: 'Web dew Portfolio',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+// colorMode module
+// definePageMeta({
+//   colorMode: 'light',
+// })
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
+
+
+
+<template>
+    <div>
+        Hello
+    </div>
+</template>
