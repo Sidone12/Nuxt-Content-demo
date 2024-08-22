bbdc660fef98d00b8aae5bd1f11881608eb9e710
Oleksii <o.katerynchuk@viseven.com>
2024-08-22 23:21:49 +0300

    add error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+ <NuxtLayout>
+    <section>
+        <h1 class="text-4xl mb-4">Upps {{ error.statusCode }}</h1>
+        <h2 lass="text-2xl mb-4">{{ error.statusMessage }}</h2>
+    </section>
+ </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+defineProps({ error: Object })
+</script>
+
+<style>
+
+</style>
--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -20,39 +20,35 @@ onMounted(() => {
     }
 
     onBeforeUnmount(() => {
-    for (const element of elements) {
-        observer.unobserve(element)
-    }
-})
+        for (const element of elements) {
+            observer.unobserve(element)
+        }
+    })
 })
 </script>
 
 <template>
-  <article
-    class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none"
-  >
-    <ContentDoc>
-      <template v-slot="{ doc }">
-        <div class="grid grid-cols-6 gap-16">
-          <div :class="{'col-span-4':doc.toc, 'col-span-6':!doc.toc}">
-            <ContentRenderer :value="doc" />
-          </div>
-          <div
-            class="col-span-2 not-prose"
-            v-if="doc.toc"
-          >
-            <aside class=" sticky top-8">
-              <div class="font-semibold md-2">Table of contents</div>
-              <nav>
-                <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
-              </nav>
-            </aside>
-          </div>
+<article class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none">
+    <ContentDoc >
+        <template #not-found>
+            <h1>Document not found (404)</h1>
+            <p>This blog post cant be found!</p>
+        </template>
+        <template v-slot="{ doc }">
+          <div class="grid grid-cols-6 gap-16">
+            <div :class="{'col-span-4':doc.toc, 'col-span-6':!doc.toc}">
+                <ContentRenderer :value="doc" />
+            </div>
+            <div class="col-span-2 not-prose" v-if="doc.toc">
+                <aside class=" sticky top-8">
+                    <div class="font-semibold md-2">Table of contents</div>
+                    <nav>
+                        <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+                    </nav>
+                </aside>
+            </div>
         </div>
-      </template>
-      <template #not-found>
-        <h1>Document not found</h1>
-      </template>
+        </template>
     </ContentDoc>
-  </article>
+</article>
 </template>
