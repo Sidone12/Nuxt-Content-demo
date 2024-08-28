4a568081dc6f36b5765ef96a4e8b866e00274283
Oleksii <o.katerynchuk@viseven.com>
2024-08-28 21:34:23 +0300

    upd [...slug] add fixes for onmounted issue wen using transition

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -25,30 +25,60 @@ onMounted(() => {
         }
     })
 })
+//  В старішій версії виникає проблема з onMounted хуком, все працює якщо ми рендеримона на сервері і тоді викликаємо хук, але якщо ми використовуємо транзишн то хук викликається раніше ніж сторінка була відрендерина!!! тому треба почекати за допомогою setTimeout
+// onMounted(() => {
+//     let elements = [];
+//     const callback = (entries) => {
+//             for (const entry of entries) {
+//                 if (entry.isIntersecting) {
+//                     activeId.value = entry.target.id
+//                     break;
+//                 }
+//             }
+//         }
+//         const observer = new IntersectionObserver(callback, {
+//             root: null,
+//             threshold: 0.5
+//         })
+
+//     setTimeout(() => {
+//         elements = document.querySelectorAll('h2, h3');
+//         for (const element of elements) {
+//             observer.observe(element)
+//         }
+
+//     }, 150);
+
+//     onBeforeUnmount(() => {
+//         for (const element of elements) {
+//             observer.unobserve(element)
+//         }
+//     })
+// })
 </script>
 
 <template>
 <article class="prose dark:prose-invert prose-pre:bg-white dark:prose-pre:bg-gray-800 prose-pre:text-gray-700 dark:prose-pre:text-gray-300 max-w-none">
-    <ContentDoc >
+    <ContentDoc>
         <template #not-found>
             <h1>Document not found (404)</h1>
             <p>This blog post cant be found!</p>
-        </template>
-        <template v-slot="{ doc }">
-          <div class="grid grid-cols-6 gap-16">
-            <div :class="{'col-span-4':doc.toc, 'col-span-6':!doc.toc}">
-                <ContentRenderer :value="doc" />
-            </div>
-            <div class="col-span-2 not-prose" v-if="doc.toc">
-                <aside class=" sticky top-8">
-                    <div class="font-semibold md-2">Table of contents</div>
-                    <nav>
-                        <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
-                    </nav>
-                </aside>
-            </div>
-        </div>
-        </template>
+</template>
+<template v-slot="{ doc }">
+<div class="grid grid-cols-6 gap-16">
+    <div :class="{'col-span-4':doc.toc, 'col-span-6':!doc.toc}">
+        <ContentRenderer :value="doc" />
+    </div>
+    <div class="col-span-2 not-prose" v-if="doc.toc">
+        <aside class=" sticky top-8">
+            <div class="font-semibold md-2">Table of contents</div>
+            <nav>
+                <TocLinks :links="doc.body.toc.links" :active-id="activeId" />
+            </nav>
+        </aside>
+    </div>
+</div>
+</template>
     </ContentDoc>
 </article>
 </template>
